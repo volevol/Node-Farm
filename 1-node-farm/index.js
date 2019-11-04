d0ae1091dd484b0fc7625b6e18fe7801b5e10074
Ivan Fedorusov <ifedorusov@gmail.com>
2019-11-04 12:21:12 +0200

    Create index.js as main doc. Templates for the Farm. DB as data.json

---
+++ 1-node-farm/index.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+//////////////////////////////////////////////////////////////
+// FILES
+
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+// const textOut = `This is what we hnow about the avocado: ${textIn}.\nCreated on  ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File has been written!');
+
+// Non-blocking, asynchronous way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     if (err) return console.log('ERROR!!!');
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3);
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('Your file has been written! c: ');
+//             });
+//         });
+//     });
+// });
+// console.log('Will read the file!')
+
+//////////////////////////////////////////////////////////////
+// SERVER
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+
+    // Overview page 
+    if (pathName === "/" || pathName === '/overview') {
+
+
+        res.end(tempOverview);
+
+    // Product page
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT');
+
+    // API 
+    } else if (pathName === '/api') {
+		
+		res.writeHead(200, {'Content-type': 'application/json'});
+		res.end(data);
+
+		// fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+		// 	const productData = JSON.parse(data);
+		// 	res.writeHead(200, {'Content-type': 'application/json'});
+		// 	res.end(data);
+		// });
+
+	} else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests  on port 8000');
+});
