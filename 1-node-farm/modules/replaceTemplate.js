4e71d826b7f29f021516dec85650b353d730ccca
Ivan Fedorusov <ifedorusov@gmail.com>
2020-04-18 18:17:40 +0300

    RESET

--- 1-node-farm/modules/replaceTemplate.js
+++
@@ -1,9 +0,0 @@
-module.exports = (temp, product) => {
-    let output = temp.replace(/{%ARTICLE_URL%}/g, product.content.article_url);
-    output = output.replace(/{%CONTENT%}/g, product.content.content);
-    output = output.replace(/{%PUBLISHED_AT%}/g, product.content.published_at);
-    output = output.replace(/{%TITLE%}/g, product.content.title);
-    output = output.replace(/{%ID%}/g, product.content.id);
-
-    return output;
-}
